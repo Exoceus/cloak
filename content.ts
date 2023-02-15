654ad7a75cdb6c3c20fefc347ad2ddc1a363cb80
Jatin Mehta <mehta.r.jatin@gmail.com>
2023-02-15 15:43:01 -0500

    much more polish

--- content.ts
+++ content.ts
@@ -15,6 +15,8 @@ const colors = {
     lineColor: { r: 0, g: 0, b: 0, a: 0 }
 }
 
+let popupOpen = false;
+
 console.log("HELLO")
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -49,17 +51,23 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
 function updateColors() {
     var root = document.querySelector(':root');
+    updateLogo()
+    updateCreateButton()
+
+    updateGreys()
     updateBackground(root)
     updateText(root)
     updateLine(root)
     updateAccent(root)
-    removeChrome()
 }
 
 function updateBackground(root) {
     let { r, g, b } = colors.backgroundColor;
     const backgroundColor = `rgb(${r},${g},${b})`
     root.style.setProperty('--surface', backgroundColor);
+    root.style.setProperty('--textfield-surface', backgroundColor);
+    root.style.setProperty('--background', backgroundColor);
+    root.style.setProperty('--mdc-theme-surface', backgroundColor);
     document.querySelector("#gb").style.backgroundColor = backgroundColor
 }
 
@@ -68,7 +76,20 @@ function updateText(root) {
     const textPrimaryColor = `rgb(${r},${g},${b})`
     const textFadedColor = `rgba(${r},${g},${b},${a})`
     root.style.setProperty('--on-surface-variant-agm', textFadedColor);
+    root.style.setProperty('--on-surface-variant', textFadedColor);
     root.style.setProperty('--on-surface', textPrimaryColor);
+    try {
+        document.querySelector("#ow77 > div > div.aCsJod.oJeWuf > div > div.Xb9hP > input").style.color = "white";
+        document.querySelector("#ow77 > div > div.aCsJod.oJeWuf > div > div.Xb9hP > input").placeholder.style.color = "white";
+    }
+    catch (e) {
+
+    }
+    try {
+        console.log("here")
+    } catch (e) {
+
+    }
 }
 
 function updateLine(root) {
@@ -79,25 +100,98 @@ function updateLine(root) {
 
 function updateAccent(root) {
     let { r, g, b, a } = colors.accentColor;
-    const accentColor = `rgb(${r},${g},${b})`
+    const accentColor = `rgb(${r},${g},${b},${0.9})`
+    const varaitnColor = `rgb(${r},${g},${b},${1})`
     root.style.setProperty('--primary', accentColor);
+    root.style.setProperty('--fab', accentColor);
+    root.style.setProperty('--now', accentColor);
+
+    // variant
+    root.style.setProperty('--fab-hover', varaitnColor);
+    root.style.setProperty('--primary-variant', varaitnColor);
+    root.style.setProperty('--on-secondary', varaitnColor);
+    root.style.setProperty('--on-secondary-variant', varaitnColor);
 }
 
-function removeChrome() {
+function updateLogo() {
+    // console.log("ehllo")
     let logoElems = document.querySelectorAll('[aria-label="Calendar"]')
     for (let i = 0; i < logoElems.length; i++) {
-        logoElems[i].style.display = "none";
+        logoElems[i].innerHTML = "<img src='https://i.ibb.co/DM99RZN/cloak-logo.png' style='height:32px;'>";
     }
-    console.log(logoElems)
+}
+
+function updateCreateButton() {
+    let plusIcon = document.querySelector("div.Gw6Zhc > svg")
+    plusIcon.style.display = 'none'
+    // console.log(plusIcon)
+
+    let createButton = document.querySelector("div.dwlvNd")
+    // console.log(createButton)
+    let buttonWrapper = document.querySelector("div.LXjtcc")
+    buttonWrapper.appendChild(createButton)
+    createButton.style.width = "100%"
+    document.querySelector("body > div.tEhMVd > div.pSp5K > div.KKOvEb > div > div.QQYuzf > div > div.LXjtcc > div > div").style.width = "90%"
+    document.querySelector("body > div.tEhMVd > div.pSp5K > div.KKOvEb > div > div.QQYuzf > div > div.LXjtcc > div > div > div").style.width = "90%"
 }
 
 
+function updateGreys() {
+    let { r, g, b, a } = colors.textColor;
+    const textPrimaryColor = `rgb(${r},${g},${b})`
+
+    console.log("ici")
+    let allElems = document.querySelectorAll("*")
+
+    allElems.forEach(elem => {
+        // var selectColor = 'rgb(0,0,0)';
+        // return (allElems.css('color') == selectColor);\
+        let cssObj = getComputedStyle(elem)
+        let cssData = []
+        for (var i = 0; i < cssObj.length; i++) {
+            if (cssObj[i] === 'color') {
+                if (cssObj.getPropertyValue(cssObj[i]) === "rgb(95, 99, 104)" || cssObj.getPropertyValue(cssObj[i]) === "rgb(60, 64, 67)" || cssObj.getPropertyValue(cssObj[i]) === "#3c4043") {
+                    elem.style.color = "var(--on-surface)";
+                }
+
+            }
+            // cssData.push(cssObj[i] + ':' + cssObj.getPropertyValue(cssObj[i]));
+        }
+        // console.log(cssData)
+    })
+}
+
 chrome.storage.local.get(null, (res) => {
-    console.log("PRIM", colors)
+    // console.log("PRIM", colors)
     colors.backgroundColor = res.backgroundColor
     colors.textColor = res.textColor
     colors.accentColor = res.accentColor
     colors.lineColor = res.lineColor
 
-    setTimeout(updateColors, 50)
+    setTimeout(updateGreys, 1000)
 })
+
+function addListeners() {
+    let logoElems = document.querySelectorAll('[aria-label="Settings menu"]')
+    for (let i = 0; i < logoElems.length; i++) {
+        logoElems[i].addEventListener("click", () => {
+            console.warn("clicked")
+            setTimeout(updateGreys, 2)
+        })
+    }
+}
+
+var timesRun = 0;
+var interval = setInterval(function () {
+    timesRun += 1;
+    updateColors()
+    if (timesRun === 30) {
+        clearInterval(interval);
+    }
+    //do whatever here..
+}, 100);
+
+
+
+// setInterval(updateGreys, 1000)
+setTimeout(addListeners, 2000)
