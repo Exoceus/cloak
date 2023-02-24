0fa77fc421caf7f8dee940006f14f418c76e1c5d
Jatin Mehta <mehta.r.jatin@gmail.com>
2023-02-23 22:19:48 -0500

    more robust screen sharing features

--- contents/helpers.ts
+++ contents/helpers.ts
@@ -6,19 +6,32 @@ export const config: PlasmoCSConfig = {
     all_frames: true
 }
 
-export function hideEventNames() {
+export function hideEventNames(genericName) {
     console.log('into ehre')
     document.querySelectorAll('span.ayClmf').forEach(elem => {
-        elem.style.color = 'transparent';
+        if (genericName) {
+            elem.innerHTML = "Some Event"
+        } else {
+            elem.style.color = 'transparent';
+        }
     })
     document.querySelectorAll('div.lFe10c > div > div.Jmftzc.gVNoLb.EiZ8Dd').forEach(elem => {
         console.warn("here")
-        elem.style.color = 'transparent';
+        if (!genericName) {
+            elem.style.color = 'transparent';
+        }
+    })
 
+    document.querySelectorAll("li > div > div.toUqff.qZvm2d-ibnC6b-bN97Pc.HRaT6d").forEach(elem => {
+        if (genericName) {
+            elem.innerHTML = '<span jsslot="" style="color: var(--on-surface);">Some Calendar</span>'
+        } else {
+            elem.style.color = 'transparent';
+        }
     })
 }
 
-// setTimeout(() => hideEventNames(), 2000)
+// setTimeout(() => hideEventNames(true), 2000)
 
 // export function addListeners() {
 //     let logoElems = document.querySelectorAll('[aria-label="Settings menu"]')
