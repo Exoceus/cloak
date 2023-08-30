35c0991c4ddb7fc211e450966dbd8913f9908f4a
Jatin Mehta <jm@Jatins-MacBook-Pro.local>
2023-08-30 16:40:50 -0400

    remove all urls plasmo config

--- contents/colorSwitching.ts
+++ contents/colorSwitching.ts
@@ -1,39 +1,39 @@
 import type { PlasmoCSConfig } from "plasmo"
+
 import { colors, updateCssVars } from "~contents"
 
 export const config: PlasmoCSConfig = {
-    matches: ["<all_urls>"],
-    run_at: "document_start",
-    all_frames: true
+  matches: ["https://calendar.google.com/*"],
+  run_at: "document_start",
+  all_frames: true
 }
 
 // note: when peeps download, its gonna be diff
 // const extension_id = "bnapdmcedmmgnkfkmdgpbmdfbgnajjea"
 
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    // if (sender.id === extension_id) {
-    if (sender.id) {
-        let { name, body } = message;
+  // if (sender.id === extension_id) {
+  if (sender.id) {
+    let { name, body } = message
 
-        if (name === "colorChange") {
-            let { color, type } = body;
-            let { r, g, b } = color;
+    if (name === "colorChange") {
+      let { color, type } = body
+      let { r, g, b } = color
 
-            if (r || g || b) {
-                let newStorage = {}
-                newStorage[type] = color
+      if (r || g || b) {
+        let newStorage = {}
+        newStorage[type] = color
 
-                chrome.storage.local.set(newStorage).then(() => {
-                    colors[type] = color;
-                    updateCssVars()
-                })
-            }
-        }
-
-        if (name === "themeChange") {
-            let { theme } = body;
-            chrome.storage.local.set({ theme })
-        }
+        chrome.storage.local.set(newStorage).then(() => {
+          colors[type] = color
+          updateCssVars()
+        })
+      }
     }
+
+    if (name === "themeChange") {
+      let { theme } = body
+      chrome.storage.local.set({ theme })
+    }
+  }
 })
